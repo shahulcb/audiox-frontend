513fe6a9d9da87de30f5e7cce1e0877d1844e25d
shahul cb <theshahulofcl@gmail.com>
2024-03-29 14:43:17 +0530

    axios instance option added

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+    baseURL: process.env.REACT_APP_BASE_URL,
+    withCredentials: true
 })
 
 export default instance
