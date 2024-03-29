f0059f4a71eab5b0d430d90dac44983c751df345
shahul cb <theshahulofcl@gmail.com>
2024-03-29 15:11:56 +0530

    test

--- src/axios.js
+++ src/axios.js
@@ -4,5 +4,9 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true
 })
+instance.interceptors.request.use(config => {
+    console.log('Request config:', config);
+    return config;
+});
 
 export default instance
